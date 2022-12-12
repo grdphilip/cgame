6d2f4cd58827efe801e3fc04d397bd9acc5d4341
Alexander Hammerman <aleha627@student.liu.se>
2022-12-12 16:11:06 +0100

    Made window appear

--- main.cc
+++ main.cc
@@ -1,6 +1,23 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-    std::cout << "Hej" << std::endl;
+    sf::VideoMode mode{640, 640};
+    sf::RenderWindow window{mode, "SFML"};
+    while(window.isOpen())
+    {
+        sf::Event event;
+        while(window.pollEvent(event))
+        {
+            if(event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+        }
+        window.clear();
+
+    window.display();
+    }
+    
 }
